a12519202365cace5f61abb6c411f230af8cf93d
Sadamhusseinmohamed <sadamkeyow2@gmail.com>
2025-10-27 04:08:35 -0700

    Assigment Oprators Update

--- Main.js
+++ Main.js
@@ -9,6 +9,14 @@
 
 
 let name = "John";
-let age = 25;
-const yearOfBirth = 1998;
+let age = 1992;
+const yearOfBirth = 2025;
 console.log(age - yearOfBirth)
+
+// assigment Opraters 
+
+let x = 5;
+let y = 10;
+x+=8;
+console.log(x);
+
