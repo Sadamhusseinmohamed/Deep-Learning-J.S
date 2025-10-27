87b85804a3212ad217e8304babd8c4533c279bf1
Sadamhusseinmohamed <sadamkeyow2@gmail.com>
2025-10-27 05:06:01 -0700

    waxaan bartay oo ku soo daray logical oprators

--- Main.js
+++ Main.js
@@ -29,3 +29,36 @@ let U=10;
 let O=59;
 result = U = O;
 console.log(result);
+
+// comparison operators
+
+// equal to ==
+// not equal to !=
+// sctrictly equal to ===
+// not strictly ===
+// greater than >
+// equal to or greater than >=
+
+
+var1 = 10;
+var2 = "10";
+console.log(30 === 30);
+
+      // logical oprators 
+// logical and &&  Meaning both conditions should be true
+// logical or || Meaning either one condition should be true
+// logical not ! Meaning opposite of the condition
+
+
+ let var3 = true && false;
+ let var4 = false || false;
+ let var5 = !true;
+
+console.log(!var3);
+console.log(var4);
+console.log(var5);
+
+
+
+
+
